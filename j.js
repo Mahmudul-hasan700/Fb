e3eebdfcd5ef05684c243095a5cc7e767617f8bc
Mahmudul-hasan700 <128229930+Mahmudul-hasan700@users.noreply.github.com>
2023-09-25 16:35:28 +0000

    A

--- j.js
+++ j.js
@@ -1,137 +1,58 @@
-function showLoadingAnimation() {
-  const inputContainer = document.querySelector(".input-container");
-  const loaderWrapper = document.getElementById("loader-wrapper");
-  inputContainer.style.display = "none";
-  loaderWrapper.style.display = "block";
+// Function to toggle FAQ answers
+function toggleAnswer() {
+  const answer = this.nextElementSibling;
+  answer.classList.toggle("hidden");
 }
 
-function showDownloadButtons() {
-  const loaderWrapper = document.getElementById("loader-wrapper");
-  const downloadButtonsContainer = document.getElementById("downloadLinks");
-
-  loaderWrapper.style.display = "none";
-  downloadButtonsContainer.style.display = "block";
-}
-
-function downloadFile(url, fileName) {
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = fileName;
-  a.click();
-}
-
-function showErrorMessage() {
-  const errorMessage = document.getElementById("fetch-error");
-  errorMessage.style.display = "block";
-  hideLoadingAnimation();
-  const inputContainer = document.querySelector(".input-container");
-  inputContainer.style.display = "block";
-
-  // Set a timer to hide the error message after 3 seconds (3000 milliseconds)
-  setTimeout(() => {
-    errorMessage.style.display = "none";
-  }, 3000);
-}
-
-function hideErrorMessage() {
-  const errorMessage = document.getElementById("fetch-error");
-  errorMessage.style.display = "none";
-}
-
-function hideLoadingAnimation() {
-  const loaderWrapper = document.getElementById("loader-wrapper");
-  loaderWrapper.style.display = "none";
-}
-
-document.getElementById("downloadButton").addEventListener("click", function() {
-  const videoUrl = document.getElementById("videoUrl").value;
-  const settings = {
-    async: true,
-    crossDomain: true,
-    url: 'https://facebook17.p.rapidapi.com/api/facebook/links', // Replace with your RapidAPI endpoint
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': '614217f6c7msh1e300360887b413p1665e8jsn4eb75dedc7fd',
-      'X-RapidAPI-Host': 'facebook17.p.rapidapi.com'
-    },
-    processData: false,
-    data: `{"url":"${videoUrl}"}`
-  };
-
-  showLoadingAnimation();
-
-
-  $.ajax(settings)
-    .done(function(response) {
-      if (response && response.length > 0) {
-        const downloadLinks = response[0].urls;
-        const downloadHDButton = document.getElementById("downloadHD");
-        const downloadSDButton = document.getElementById("downloadSD");
-
-        // Clear previous download links
-        downloadHDButton.style.display = "none";
-        downloadSDButton.style.display = "none";
-
-        // Initialize variables to store thumbnail URL and title
-        let thumbnailUrl = "";
-        let videoTitle = "";
-
-        // Display download links for HD and SD separately
-        downloadLinks.forEach(linkInfo => {
-          if (linkInfo.subName.toLowerCase() === "hd") {
-            downloadHDButton.style.display = "block";
-            downloadHDButton.setAttribute("data-url", linkInfo.url);
-            downloadHDButton.setAttribute("data-extension", linkInfo.extension);
-          } else if (linkInfo.subName.toLowerCase() === "sd") {
-            downloadSDButton.style.display = "block";
-            downloadSDButton.setAttribute("data-url", linkInfo.url);
-            downloadSDButton.setAttribute("data-extension", linkInfo.extension);
-          }
-
-          // Check for thumbnail and title
-          if (linkInfo.subName.toLowerCase() === "thumbnail") {
-            thumbnailUrl = linkInfo.url;
-          } else if (linkInfo.subName.toLowerCase() === "title" && linkInfo.title) {
-            videoTitle = linkInfo.title;
-          }
-        });
-
-        // Display thumbnail
-        const thumbnail = document.getElementById("thumbnail");
-        thumbnail.src = thumbnailUrl;
-
-        // Display title
-        const videoTitleElement = document.getElementById("videoTitle");
-        videoTitleElement.textContent = videoTitle;
-
-        // Show the video info container
-        const videoInfoContainer = document.getElementById("videoInfo");
-        videoInfoContainer.style.display = "block";
-
-        // Show the download buttons and hide the error message
-        showDownloadButtons();
-        hideErrorMessage();
-      } else {
-        // Show the error message when there's no response or response is false
-        showErrorMessage();
-      }
-    })
-    .fail(function() {
-      // Show the error message when the AJAX request fails
-      showErrorMessage();
-    });
-});
-document.getElementById("downloadHD").addEventListener("click", function() {
-  const url = this.getAttribute("data-url");
-  const extension = this.getAttribute("data-extension");
-  const fileName = "video_hd" + extension;
-  downloadFile(url, fileName);
+// Add click event listeners to all question elements
+const questions = document.querySelectorAll(".question .toggle");
+questions.forEach(question => {
+  question.addEventListener("click", toggleAnswer);
 });
 
-document.getElementById("downloadSD").addEventListener("click", function() {
-  const url = this.getAttribute("data-url");
-  const extension = this.getAttribute("data-extension");
-  const fileName = "video_sd" + extension;
-  downloadFile(url, fileName);
+const shareButton = document.querySelector(".share-button");
+
+shareButton.addEventListener("click", e => {
+  if (navigator.share) {
+    navigator
+      .share({ title: "Share Facebook Video Downloader", url: window.location.href })
+      .then(() => {
+        console.log("Shared successfully");
+      })
+      .catch(error => {
+        console.error("Error sharing:", error);
+      });
+  } else {
+    // Fallback for browsers that don't support navigator.share
+    const shareURL = encodeURIComponent(window.location.href);
+    const shareText = encodeURIComponent("Check out this Facebook Video Downloader!");
+    const socialMediaURL = `https://twitter.com/intent/tweet?url=${shareURL}&text=${shareText}`;
+
+    // Open a new window with the social media sharing link
+    window.open(socialMediaURL, "_blank");
+  }
+});
+
+// Get references to the input field and the paste button
+const inputField = document.getElementById('videoUrl'); // Assuming your input field has the id 'params'
+const closePasteButton = document.querySelector('.close-button'); // Unique variable name
+
+// Add an event listener to the input field for input changes
+inputField.addEventListener('input', function() {
+  const inputValue = inputField.value;
+
+  // Check if the input value is empty
+  if (inputValue === '') {
+    // If empty, hide the close icon
+    closePasteButton.innerHTML = '';
+  } else {
+    // If not empty, display the close icon
+    closePasteButton.innerHTML = '<i class="fas fa-times"></i>';
+  }
+});
+
+// Function to clear the input field when the close icon is clicked
+closePasteButton.addEventListener('click', function() {
+  inputField.value = ''; // Clear the input field
+  closePasteButton.innerHTML = ''; // Hide the close icon again
 });
--- script.js
+++ script.js
@@ -1,89 +1,159 @@
-// Function to hide the input container and show the video download buttons
-function showDownloadButtons() {
-  const inputContainer = document.querySelector(".input-container");
-  const downloadButtonsContainer = document.getElementById("downloadLinks");
-  const downloadAnotherButton = document.getElementById("downloadAnotherButton");
+// Get references to the input field and download button
+const videoUrlInput = document.getElementById('videoUrl');
+const downloadButton = document.getElementById('downloadButton');
+const errorMessage = document.querySelector('.error-message');
+const thumbnailImg = document.getElementById('thumbnail'); // Added reference to thumbnail image
 
-  inputContainer.style.display = "none";
-  downloadButtonsContainer.style.display = "block";
-  downloadAnotherButton.style.display = "block";
+// Function to check if a URL is a valid Facebook video link
+function isValidFacebookVideoLink(url) {
+  return url.includes('facebook.com') || url.includes('fb.watch');
 }
 
-document.getElementById("downloadButton").addEventListener("click", function() {
-  const videoUrl = document.getElementById("videoUrl").value;
-  const settings = {
-    async: true,
-    crossDomain: true,
-    url: 'https://facebook17.p.rapidapi.com/api/facebook/links',
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': '3310140644msh1db7d0febe86dabp1bec8bjsn72eab7657ff6',
-      'X-RapidAPI-Host': 'facebook17.p.rapidapi.com'
-    },
-    processData: false,
-    data: `{"url":"${videoUrl}"}`
-  };
+// Add an event listener to the input field to check when it changes
+videoUrlInput.addEventListener('input', () => {
+  const videoUrl = videoUrlInput.value;
 
-  $.ajax(settings).done(function(response) {
-    const downloadLinks = response[0].urls;
-    const downloadHDButton = document.getElementById("downloadHD");
-    const downloadSDButton = document.getElementById("downloadSD");
-
-    // Clear previous download links
-    downloadHDButton.style.display = "none";
-    downloadSDButton.style.display = "none";
-
-    // Display download links for HD and SD separately
-    downloadLinks.forEach(linkInfo => {
-      if (linkInfo.subName.toLowerCase() === "hd") {
-        downloadHDButton.style.display = "block";
-        downloadHDButton.setAttribute("data-url", linkInfo.url);
-        downloadHDButton.setAttribute("data-extension", linkInfo.extension);
-      } else if (linkInfo.subName.toLowerCase() === "sd") {
-        downloadSDButton.style.display = "block";
-        downloadSDButton.setAttribute("data-url", linkInfo.url);
-        downloadSDButton.setAttribute("data-extension", linkInfo.extension);
-      }
-    });
-
-    // Show the download buttons and "Download Another Video" button
-    showDownloadButtons();
-  });
+  // Check if the entered URL is a valid Facebook video link
+  if (isValidFacebookVideoLink(videoUrl)) {
+    // Enable the download button
+    downloadButton.disabled = false;
+    // Hide the error message
+    errorMessage.classList.add('hidden');
+  } else {
+    // Disable the download button
+    downloadButton.disabled = true;
+    // Show the error message
+    errorMessage.classList.remove('hidden');
+  }
 });
 
-// Add click event listeners for the download buttons
-document.getElementById("downloadHD").addEventListener("click", function() {
-  const url = this.getAttribute("data-url");
-  const extension = this.getAttribute("data-extension");
-  const fileName = "video_hd" + extension;
-  downloadFile(url, fileName);
-});
+// Function to toggle the input field for paste button
+function toggleInput() {
+  videoUrlInput.select();
+  document.execCommand('paste');
+}
 
-document.getElementById("downloadSD").addEventListener("click", function() {
-  const url = this.getAttribute("data-url");
-  const extension = this.getAttribute("data-extension");
-  const fileName = "video_sd" + extension;
-  downloadFile(url, fileName);
-});
+// Add an event listener to the paste button
+const pasteButton = document.querySelector('.btn-paste');
+pasteButton.addEventListener('click', toggleInput);
 
-// Function to trigger the download
+// Function to show the loading animation
+function showLoadingAnimation() {
+  const inputContainer = document.getElementById('input-container');
+  const loaderWrapper = document.getElementById('loader-wrapper');
+
+  if (inputContainer && loaderWrapper) {
+    inputContainer.style.display = 'none';
+    loaderWrapper.style.display = 'block';
+  }
+}
+
+// Function to show the download buttons
+function showDownloadButtons() {
+  document.getElementById('loader-wrapper').style.display = 'none';
+  document.getElementById('downloadLinks').style.display = 'block';
+}
+
+// Function to download a file
 function downloadFile(url, fileName) {
-  const a = document.createElement("a");
+  const a = document.createElement('a');
   a.href = url;
   a.download = fileName;
   a.click();
 }
 
-// Add click event listener for the "Download Another Video" button
-document.getElementById("downloadAnotherButton").addEventListener("click", function() {
-  // Reset the input field and hide the download buttons
-  const inputContainer = document.querySelector(".input-container");
-  const downloadButtonsContainer = document.getElementById("downloadLinks");
-  const downloadAnotherButton = document.getElementById("downloadAnotherButton");
+// Function to show the error message and hide loader animation
+function showErrorMessage() {
+  const errorMessage = document.getElementById('fetch-error');
+  errorMessage.style.display = 'block';
+  document.getElementById('input-container').style.display = 'block';
+  
+  // Hide the loader animation
+  hideLoadingAnimation();
 
-  document.getElementById("videoUrl").value = "";
-  inputContainer.style.display = "block";
-  downloadButtonsContainer.style.display = "none";
-  downloadAnotherButton.style.display = "none";
+  setTimeout(() => {
+    errorMessage.style.display = 'none';
+  }, 3000);
+}
+
+// Function to hide the error message
+function hideErrorMessage() {
+  document.getElementById('fetch-error').style.display = 'none';
+}
+
+// Function to hide the loading animation
+function hideLoadingAnimation() {
+  document.getElementById('loader-wrapper').style.display = 'none';
+}
+
+// Add a click event listener to the download button
+downloadButton.addEventListener('click', function () {
+  const videoUrl = document.getElementById('videoUrl').value;
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: 'https://facebook-reel-and-video-downloader.p.rapidapi.com/app/main.php?url=' + encodeURIComponent(videoUrl),
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '3310140644msh1db7d0febe86dabp1bec8bjsn72eab7657ff6',
+      'X-RapidAPI-Host': 'facebook-reel-and-video-downloader.p.rapidapi.com',
+    },
+  };
+
+  showLoadingAnimation();
+
+  $.ajax(settings)
+    .done(function (response) {
+      if (response.success) {
+        const downloadLinks = response.links;
+        const downloadHDButton = document.getElementById('downloadHD');
+        const downloadSDButton = document.getElementById('downloadSD');
+
+        downloadHDButton.style.display = 'none';
+        downloadSDButton.style.display = 'none';
+
+        // Check if the API response includes a thumbnail URL
+        if (response.thumbnail) {
+          thumbnailImg.src = response.thumbnail;
+          thumbnailImg.style.display = 'block'; // Display the thumbnail image
+        }
+
+        for (const link in downloadLinks) {
+          if (link.includes('Low Quality')) {
+            downloadSDButton.style.display = 'block';
+            downloadSDButton.addEventListener('click', function () {
+              downloadFile(downloadLinks[link], 'video_sd.mp4');
+            });
+          } else if (link.includes('High Quality')) {
+            downloadHDButton.style.display = 'block';
+            downloadHDButton.addEventListener('click', function () {
+              downloadFile(downloadLinks[link], 'video_hd.mp4');
+            });
+          }
+        }
+
+        showDownloadButtons();
+        hideErrorMessage();
+      } else {
+        showErrorMessage();
+      }
+    })
+    .fail(function () {
+      showErrorMessage();
+    });
+});
+
+// Add click event listeners for the download buttons
+document.getElementById('downloadHD').addEventListener('click', function () {
+  const url = this.getAttribute('data-url');
+  const extension = this.getAttribute('data-extension');
+  const fileName = 'video_hd' + extension;
+  downloadFile(url, fileName);
+});
+
+document.getElementById('downloadSD').addEventListener('click', function () {
+  const url = this.getAttribute('data-url');
+  const extension = this.getAttribute('data-extension');
+  const fileName = 'video_sd' + extension;
+  downloadFile(url, fileName);
 });
