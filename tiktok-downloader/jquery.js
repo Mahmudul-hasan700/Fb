10193d5fa18b99657753084fd8e5a24e7812132c
Mahmudul-hasan700 <128229930+Mahmudul-hasan700@users.noreply.github.com>
2023-09-30 05:48:51 +0000

    A

--- tiktok-downloader/jquery.js
+++ tiktok-downloader/jquery.js
@@ -19,26 +19,17 @@ shareButton.addEventListener("click", e => {
   }
 });
 
-// Get references to the input field and the paste button
-const inputField = document.getElementById('URL'); // Assuming your input field has the id 'params'
+const inputField = document.getElementById('videoUrl');
 const pasteButton = document.querySelector('.close-button');
-
-// Add an event listener to the input field for input changes
 inputField.addEventListener('input', function() {
   const inputValue = inputField.value;
-
-  // Check if the input value is empty
   if (inputValue === '') {
-    // If empty, hide the close icon
     pasteButton.innerHTML = '';
   } else {
-    // If not empty, display the close icon
     pasteButton.innerHTML = '<i class="fas fa-times"></i>';
   }
 });
-
-// Function to clear the input field when the close icon is clicked
 pasteButton.addEventListener('click', function() {
-  inputField.value = ''; // Clear the input field
-  pasteButton.innerHTML = ''; // Hide the close icon again
+  inputField.value = '';
+  pasteButton.innerHTML = '';
 });
--- tiktok-downloader/script.js
+++ tiktok-downloader/script.js
@@ -1,186 +1,179 @@
-var BLOBS = {}
-var DATA
-var RETRIES = {}
+const BLOBS = {};
+const RETRIES = {};
 
 const startLoading = () => {
-  const loader = document.getElementById('overlay_loader')
-  loader.classList.remove("hidden")
+  const loader = document.getElementById('overlay_loader');
+  loader.classList.remove("hidden");
 }
+
 const stopLoading = () => {
-  const loader = document.getElementById('overlay_loader')
-  loader.classList.add("hidden")
+  const loader = document.getElementById('overlay_loader');
+  loader.classList.add("hidden");
 }
+
 const showPageContent = () => {
-  const container = document.getElementById("container")
-  container.classList.remove('hidden')
-  const card = document.getElementById("card")
-  card.classList.add('hidden')
-  const bodyitem = document.getElementById("body-item")
-  bodyitem.classList.add('hidden')
+  const container = document.getElementById("container");
+  container.classList.remove('hidden');
+  const card = document.getElementById("card");
+  card.classList.add('hidden');
 }
 
-const readUrl = () => {
-  const url = new URL(window.location.href)
-  const targetUrlValue = url.searchParams.get('url')
-  if (targetUrlValue === null) {
-
-    return false
-  }
-  return (targetUrlValue)
-}
-const makeRequest = () => {
-  const url = readUrl();
-  const endpoint = "https://tikwm.com/api/?url=";
+const makeRequest = async () => {
+  const url = document.getElementById('videoUrl').value;
   if (url) {
-    const requestURL = endpoint + url;
-    fetch(requestURL)
-      .then((response) => {
-        if (response.status >= 200 && response.status <= 299) {
-          return response.json();
-        } else {
-          renderOverlayErrorMessage("Server returned Error code : " + response.status, response.statusText);
-          return null;
-        }
-      })
-      .then((data) => processResponse(data))
-      .catch((e) => {
-        renderOverlayErrorMessage("Error Occurred", e.message);
-        throw e;
-      });
+    startLoading();
+    try {
+      const response = await fetch(`https://tikwm.com/api/?url=${url}`);
+
+      if (response.status >= 200 && response.status <= 299) {
+        const data = await response.json();
+        processResponse(data);
+      } else {
+        renderOverlayErrorMessage("Server returned Error code : " + response.status, response.statusText);
+      }
+    } catch (error) {
+      renderOverlayErrorMessage("Error Occurred", error.message);
+      throw error;
+    }
+  } else {
+    alert("Please enter a valid video URL.");
   }
 };
 
 const processResponse = (resJSON) => {
-  if (!resJSON) { return false }
-  const resCode = resJSON['code']
-  const resMsg = resJSON['msg']
+  if (!resJSON) {
+    showError("Failed to fetch video."); // Show error if response is empty
+    return;
+  }
+
+  const resCode = resJSON['code'];
+  const resMsg = resJSON['msg'];
+
   if (resCode === -1) {
-    renderOverlayErrorMessage("Failed to fetch video.", resMsg)
+    showError("Failed to fetch video.", resMsg); // Show error with custom message
   } else if (resCode === 0) {
-    DATA = resJSON['data']
-    getMediaBlobs(resJSON['data'])
-    renderPage(resJSON['data'])
-    stopLoading()
-  }
-  else {
-    console.error("UNKNOWN RESPONSE CODE.")
-    console.log(resJSON)
+    const data = resJSON['data'];
+    renderPage(data);
+    stopLoading();
+  } else {
+    console.error("UNKNOWN RESPONSE CODE.");
+    console.log(resJSON);
   }
 }
+
 const renderPage = (data) => {
-  renderData(data)
-  showPageContent()
+  renderData(data);
+  showPageContent();
+  getMediaBlobs(data); // Fetch media blobs when rendering the page.
 }
+
 const renderData = (data) => {
   const videoThumbnailField = document.getElementById('video_thumbnail');
   const videoTagField = document.getElementById('video_tag');
   const videoPoster = data['origin_cover'];
   const videoTitle = data['title'];
 
-const videoDownloadField = document.getElementById('video_download')
-    const videoDownloadWMField = document.getElementById('video_download_wm')
-  
-  videoThumbnailField.src = videoPoster; // Set the thumbnail image URL
-  videoTagField.innerText = videoTitle; // Set the video tag
+  // Display the thumbnail image, video title, etc., in their respective places
+  videoThumbnailField.src = videoPoster;
+  videoTagField.innerText = videoTitle;
 }
-const getMediaBlobs = (data) => {
-  console.log("getting media blobs")
-  
-  const video = data['play']
-  const video_wm = data['wmplay']
-  const music = data['music']
 
-  getBlob("video", video)
-  getBlob("music", music)
-  getBlob("video_wm", video_wm)
-}
-const getBlob = (blobKey, url) => {
-  const xhr = new XMLHttpRequest()
-  xhr.open("GET", url)
-  xhr.responseType = 'blob';
-  xhr.onreadystatechange = function() {
-    if (this.readyState === this.DONE) {
-      console.log('DONE: ', this.status);
-      if (this.status == 200) {
-        var blob = new Blob([this.response]);
-        console.log("got blob " + blobKey)
-        BLOBS[blobKey] = blob
-      } else {
-        console.log("failed to get blob. trying again.")
-        !(blobKey in RETRIES) && (RETRIES[blobKey] = 0)
-        if (RETRIES[blobKey] < 50) {
-          RETRIES[blobKey]++
-          getBlob(blobKey, url)
-        }
-      }
-    }
-  }
-  xhr.send()
-}
 const downloadFileFromBlob = (blobKey, filename, extension) => {
-  console.log("downloading blob " + blobKey)
-  const blob = BLOBS[blobKey]
-  console.log(Boolean(blob))
+  const blob = BLOBS[blobKey];
+  if (!blob) {
+    console.error(`Blob with key '${blobKey}' not found.`);
+    return;
+  }
+
   const url = window.URL.createObjectURL(blob);
   const a = document.createElement("a");
 
   a.style = "display: none";
   document.body.appendChild(a);
   a.href = url;
-  a.download = filename + extension;
+  a.download = filename + "." + extension;
 
   a.click();
-  console.log(url)
   window.URL.revokeObjectURL(url);
-  a.remove()
+  a.remove();
 }
+
+const getBlob = (blobKey, url) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.responseType = 'blob';
+  xhr.onreadystatechange = function() {
+    if (this.readyState === this.DONE) {
+      if (this.status == 200) {
+        const blob = new Blob([this.response]);
+        BLOBS[blobKey] = blob;
+      } else {
+        console.log(`Failed to get blob '${blobKey}'. Trying again.`);
+        !(blobKey in RETRIES) && (RETRIES[blobKey] = 0);
+        if (RETRIES[blobKey] < 50) {
+          RETRIES[blobKey]++;
+          getBlob(blobKey, url);
+        }
+      }
+    }
+  };
+  xhr.send();
+}
+
+const getMediaBlobs = (data) => {
+  const video = data['play'];
+  const video_wm = data['wmplay'];
+  const music = data['music'];
+
+  getBlob("video", video);
+  getBlob("music", music);
+  getBlob("video_wm", video_wm);
+}
+
+document.getElementById('downloadBtn').addEventListener('click', () => {
+  makeRequest();
+});
+
+document.getElementById('video_download').addEventListener('click', (e) => {
+  handleDownloadButtonClick(e.target);
+});
+
+document.getElementById('music_download').addEventListener('click', (e) => {
+  handleDownloadButtonClick(e.target);
+});
+
 const handleDownloadButtonClick = (button) => {
-  console.log("clicked button")
-  const blobKey = button.getAttribute('key')
-  toggleButtonLoading(button)
-  waitForBlobToDownload(blobKey, button)
+  const blobKey = button.getAttribute('key');
+  const videoTitle = document.getElementById('video_tag').innerText;
+  const fileName = sanitizeFileName(videoTitle);
+
+  toggleButtonLoading(button);
+  waitForBlobToDownload(blobKey, button, fileName);
 }
-const waitForBlobToDownload = (blobKey, button) => {
-  console.log("looking for blob")
+
+const waitForBlobToDownload = (blobKey, button, fileName) => {
   if (!BLOBS.hasOwnProperty(blobKey)) {
-    console.log("no blob found. recalling ...")
-    setTimeout(waitForBlobToDownload, 100, blobKey, button)
+    setTimeout(waitForBlobToDownload, 100, blobKey, button, fileName);
   } else {
-    console.log("Found blob")
-    let filename = DATA['author']['unique_id'] + "___" + DATA['title'] + "___"
-    if (blobKey == "video") {
-      filename += "NO_WM"
-      var extension = ".mp4"
-    }
-    else if (blobKey == "music") {
-      filename = DATA["music_info"]['author'] + "___" + DATA["music_info"]['title']
-      var extension = ".mp3"
-    }
-    downloadFileFromBlob(blobKey, filename, extension)
-    toggleButtonLoading(button)
+    downloadFileFromBlob(blobKey, fileName, blobKey === "music" ? "mp3" : "mp4");
+    toggleButtonLoading(button);
   }
 }
+
+const sanitizeFileName = (fileName) => {
+  // Replace characters that are not allowed in file names
+  return fileName.replace(/[<>:"/\\|?*]/g, '_');
+}
+
 const toggleButtonLoading = (button) => {
-  console.log("toggled button state")
-  const loader = button.querySelector("#loader")
-  const text = button.querySelector(".button-content")
-  const loading = !(loader.classList.contains('hidden'))
-
-  loader.classList.toggle('hidden')
-  text.classList.toggle('opacity-0')
-
+  const loader = button.querySelector("#loader");
+  const text = button.querySelector(".button-content");
+  const loading = !(loader.classList.contains('hidden'));
+  loader.classList.toggle('hidden');
+  text.classList.toggle('opacity-0');
   if (loading) {
-    button.style.pointerEvents = "all"
+    button.style.pointerEvents = "all";
   } else {
-    button.style.pointerEvents = "none"
+    button.style.pointerEvents = "none";
   }
 }
-const KMBFormat = (num) => {
-  return Intl.NumberFormat('en-US', {
-    notation: "compact",
-    maximumFractionDigits: 1
-  }).format(num)
-}
-
-startLoading()
-makeRequest()
